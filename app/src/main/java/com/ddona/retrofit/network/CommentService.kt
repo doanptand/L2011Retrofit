56c97a3173abffa5260208a23443bb18fa897895
DoanPT1 <doanptand@gmail.com>
2021-11-21 20:24:34 +0700

    update, delete with retrofit

--- app/src/main/java/com/ddona/retrofit/network/CommentService.kt
+++ app/src/main/java/com/ddona/retrofit/network/CommentService.kt
@@ -3,8 +3,7 @@ package com.ddona.retrofit.network
 import com.ddona.retrofit.model.Comment
 import io.reactivex.rxjava3.core.Observable
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface CommentService {
 
@@ -19,4 +18,25 @@ interface CommentService {
 
     @GET("comments/{id}")
     fun getCommentById(@Path("id") postId: String): Call<Comment>
+
+    @POST("posts")
+    fun addComment(@Body comment: Comment)
+
+    //    https://jsonplaceholder.cypress.io/comment?user_id=5&field=id&sort=desc
+    @GET("comments")
+    suspend fun getCommentsWithQuery(
+        @Query("user_id") userId: Int,
+        @Query("field") searchField: String,
+        @Query("sort") sortType: String
+    ): List<Comment>
+
+
+    @PUT("comment/{id}")
+    suspend fun updateComment(@Path("id") id: Int, @Body comment: Comment)
+
+    @PATCH("comment/{id}")
+    suspend fun updateComment2(@Path("id") id: Int, @Body comment: Comment)
+
+    @DELETE("comment/{id}")
+    suspend fun deleteComment(@Path("id") id: Int)
 }
