de30cc69f4879931bfe43401662e25550c574a9f
DoanPT1 <doanptand@gmail.com>
2021-11-21 20:37:59 +0700

    use interceptor to add header and logging

--- app/src/main/java/com/ddona/retrofit/network/CommentClient.kt
+++ app/src/main/java/com/ddona/retrofit/network/CommentClient.kt
@@ -1,12 +1,38 @@
 package com.ddona.retrofit.network
 
 import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object CommentClient {
 
+
+    private val headerInterceptor = Interceptor {
+        val originRequest = it.request()
+        val newRequest = originRequest.newBuilder()
+            .header("Authorization", "this is jwt key")
+            .build()
+        it.proceed(newRequest)
+    }
+
+    private fun getLoggingInterceptor(): HttpLoggingInterceptor {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return loggingInterceptor
+    }
+
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(headerInterceptor)
+        .addInterceptor(getLoggingInterceptor())
+        .build()
+
+
     private val retrofit = Retrofit.Builder()
+        .client(okHttpClient)
         .baseUrl("https://jsonplaceholder.cypress.io/")
         .addConverterFactory(GsonConverterFactory.create())
         .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
